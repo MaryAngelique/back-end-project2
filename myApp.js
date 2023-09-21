c1d3d2612922d46fca4b357014339438176f514c
Angelique Abacajan <ma_utsey@yahoo.com>
2023-09-20 23:52:52 -0400

    Implemented start working on express server to log hello express with app.method(path, handler)

--- myApp.js
+++ myApp.js
@@ -1,7 +1,14 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
 
-console.log("Hello World ")
+app.get("/", function(request, response) {
+    response.send('Hello Express');
+});
+
+app.listen(3001, function() {
+    console.log('Listening on port 3000');
+});
 
 module.exports = app;
